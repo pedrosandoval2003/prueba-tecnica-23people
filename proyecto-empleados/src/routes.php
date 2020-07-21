a4f4d7794e97779c70278675851580da79d4bf16
Pedro Sandoval <pedrosandoval2003@gmail.com>
2020-07-21 08:51:33 -0400

    routes added version and people

--- proyecto-empleados/src/routes.php
+++ proyecto-empleados/src/routes.php
@@ -5,6 +5,20 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 return function (App $app) {
+// rutas
+$app->group('/api', function () use ($app) {
+  // Version
+  $app->group('/v1', function () use ($app) {
+    $app->get('/people', 'ObtenerPersonas');
+    $app->get('/people/{nationalId}', 'ObtenerPersona');
+    $app->post('/people', 'AgregarPersona');
+    $app->put('/people/{nationalId}', 'ActualizarPersona');
+    $app->delete('/people/{nationalId}', 'EliminarPersona');
+  });
+});
+
+
+
     $container = $app->getContainer();
 
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
